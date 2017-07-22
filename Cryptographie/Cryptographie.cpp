0c5cab3637740db3979ad06f70a37bfec4f98900
Abdoul Aziz Mahamadou Ba <azmaba@gmail.com>
2017-07-22 17:21:38 +0200

    Ecriture des fonctions de base qui vont etres remplies plutard

--- Cryptographie/Cryptographie.cpp
+++ Cryptographie/Cryptographie.cpp
@@ -2,10 +2,51 @@
 //
 
 #include "stdafx.h"
+/////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Foncteur qui effectue un chiffrement par decalage
+class ChiffrementDecal {
+
+public:
+
+	// Constructeur prenant le decalage voulu en argument
+	ChiffrementDecal(int decalage)
+	{
+		// A vous de jouer...
+	}
+
+
+	// A vous de jouer...
+
+private:
+
+	int m_decalage; // Le decalage a appliquer au texte
+
+};
 
 
 int main()
 {
-    return 0;
+
+	// Le message a crypter
+	string texte("BIENVENUE SUR LE MOOC C++ D'OCR !!");
+
+	// Demande du decalage a l'utilisateur
+	cout << "Quel decalage voulez-vous utiliser ? ";
+	int decalage;
+	cin >> decalage;
+
+	// Creation du foncteur
+	ChiffrementDecal foncteur(decalage);
+
+	// Chaine de caracteres pour le message crypter
+	string texte_crypte;
+
+	// A vous de jouer.... 
+
+	return 0;
 }
 
